e1d9458d0462e48f1c39b6413ee23fad11136d23
Quinn Lee <leekeunhwi@gmail.com>
2017-02-01 18:24:16 -0500

    euler 56

---
+++ euler056.py
@@ -0,0 +1,23 @@
+'''
+A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+'''
+
+from heapq import heappush, heappop
+
+if __name__ == "__main__":
+
+    heap = []
+
+    for a in range(1,101):
+        for b in range(1,101):
+            num = a**b
+            total = -1 * sum([int(_)for _ in str(num)])
+            heappush(heap, total)
+
+    print(heappop(heap) * -1)
+
+
+
+
